f04525841b4ec0bc470f375d0d570b5239c66083
Etienne Bonanno <etibon@gmail.com>
2023-03-01 20:10:11 +0100

    Clean up output

--- rexync.py
+++ rexync.py
@@ -10,11 +10,13 @@ import re
 
 def load_config():
     # First check in current dir and if not found in /etc
+    conf_path="rexync_conf.yml"
     try:
-        stream = open("rexync_conf.yml", "r")
+        stream = open(conf_path, "r")
     except:
         try:
-            stream = open("/etc/rexync_conf.yml", "r")
+            conf_path="/etc/rexync_conf.yml"
+            stream = open(conf_path, "r")
         except:
             print("Unable to open rexync_conf.yml")
             exit(-1)
@@ -26,15 +28,13 @@ def load_config():
     yaml_conf = load(stream, Loader=yaml.FullLoader)["rexync"]
 
     for rule in yaml_conf["rules"]:
-        print(rule)
         pattern = rule["rule"]["pattern"]
         description = rule["rule"]["description"]
         capture_groups = rule["rule"]["capture_groups"]
 
         rules[pattern] = { "description": description, "capture_groups": capture_groups }
 
-    print(yaml_conf)
-    print(rules)
+    print(f"Loaded {len(rules)} rules from {conf_path}")
 
 def match_name(f_name):
     for key in rules:
@@ -43,11 +43,7 @@ def match_name(f_name):
             desc = rules[key]["description"]
             cgroups = rules[key]["capture_groups"]
 
-            for g in res.groups():
-                print(g)
-
             info = { n: v for (n, v) in zip(cgroups, [g for g in res.groups()])}
-            print(info)
 
             return info
 
@@ -60,17 +56,27 @@ def copy_file(from_dir, file_name, metadata):
     dest_dir = os.path.join(dest_base_dir, metadata["title"], f"Season {season}")
     os.makedirs(dest_dir, exist_ok=True)
     dest_link = os.path.join(dest_dir, file_name)
-    os.link(link_src, dest_link)
+
+    if not os.path.exists(dest_link):
+        os.link(link_src, dest_link)
+        print(f"Created link from {link_src} to {dest_link}.")
+    else:
+        print(f"File already linked in destination {dest_link} - skipped.")
+
 
 def process_dir(dir_name):
+    print(f"Processing folder {dir_name} ...")
     onlyfiles = [f for f in listdir(dir_name) if isfile(join(dir_name, f))]
 
+    processed_files=[]
     for file in onlyfiles:
         res = match_name(file)
         if res:
+            print(f"Identified match for file {file}...")
             copy_file(dir_name, file, res)
+            processed_files.append(file)
 
-    return onlyfiles
+    return processed_files
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -80,5 +86,5 @@ if __name__ == '__main__':
         print("Please provide an input directory.")
         exit(-1)
 
-    print(process_dir(sys.argv[1]))
+    process_dir(sys.argv[1])
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
